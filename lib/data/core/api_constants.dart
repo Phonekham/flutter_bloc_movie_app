a0ce6d1487061af384df02747225974c40dcea98
Phonekham <phone.madridista@gmail.com>
2022-08-02 01:10:39 +0700

    datasource

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  ApiConstants._();
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "632e7e9a539b7b3b25658edf39bfc886";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w/w500";
+}
